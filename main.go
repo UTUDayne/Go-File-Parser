a39abf28318133a086b16ffbb01c73dad480a875
Dayne Wyler <catdragon1501@gmail.com>
2026-05-15 12:59:02 -0600

    I made a file

--- main.go
+++ main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 )
 
 var i int = 5
 
 func main() {
+	b := []byte(fmt.Sprintf("%d", i))
+	err := os.WriteFile("test.txt", b, 0644)
+	panic_func(err)
 	i = i + rand.Intn(10)
+	file, err := os.ReadFile("test.txt")
+	fmt.Println(string(file))
+}
 
-	fmt.Println("Hello World")
+func panic_func(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
