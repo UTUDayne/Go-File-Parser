1bdc369dda775e13501b7588e044f871f9ca043f
Dayne Wyler <catdragon1501@gmail.com>
2026-05-15 14:11:18 -0600

    Updated to better overwrite file

--- main.go
+++ main.go
@@ -11,13 +11,17 @@ var i int = 5
 var words = " words words words"
 
 func main() {
-	b := []byte(fmt.Sprintf("%d", i) + words)
-	err := os.WriteFile("test.txt", b, 0644)
+	byteString := []byte(fmt.Sprintf("%d", i) + words + "\n")
+	file, err := os.OpenFile("test.txt", os.O_APPEND|os.O_CREATE, 0644)
 	panic_func(err)
+	defer file.Close()
 	i = i + rand.Intn(10)
-	file, err := os.ReadFile("test.txt")
+	os.WriteFile("test.txt", []byte(""), 0644)
+	_, err = file.Write(byteString)
 	panic_func(err)
-	fmt.Println(string(file))
+	content, err := os.ReadFile("test.txt")
+	panic_func(err)
+	fmt.Println(string(content))
 }
 
 func panic_func(err error) {
